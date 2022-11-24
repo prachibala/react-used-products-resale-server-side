ff5986596693a68417c3bb13410fb086fe811a65
Prachi Bala <prachibala330@gmail.com>
2022-11-25 03:44:44 +0600

    ver-1.1

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const port = process.env.PORT || 5000;
 // middle wares
 app.use(cors());
 app.use(express.json());
-
+const categories = require("./categories.json");
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+app.get("/categories", (req, res) => {
+    res.send(categories);
 });
 
 app.listen(port, () => {
